64a098489e931d79465941b78fb090291eeaca19
SSS <CHNxy_SSS@qq.com>
2017-11-27 20:33:20 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/cn/imust/sssweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package cn.imust.sssweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * 和服务器的交互
+ * Created by Shinelon on 2017/11/27 0027.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
